51907f40f0487158f47ffca5c90c5cf4a5e1f917
ghtyn <81617033+ghtyn@users.noreply.github.com>
2021-05-11 09:47:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,13 +16,13 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #主题
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-#git clone https://github.com/garypang13/luci-theme-edge  package/luci-theme-edge
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone https://github.com/garypang13/luci-theme-edge  package/luci-theme-edge
 #插件
-git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
-git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
+#git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
+#git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
-git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
-git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+#git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
+#git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-git clone -b lede https://github.com/pymumu/luci-app-smartdns package/lean/luci-app-smartdns
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns package/lean/luci-app-smartdns
