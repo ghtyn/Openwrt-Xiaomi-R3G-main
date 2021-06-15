493463f5b3b0adcd4b053350e3bc33919bc1c5c1
ghtyn <81617033+ghtyn@users.noreply.github.com>
2021-06-15 12:39:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -26,3 +26,4 @@ git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
 git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone -b lede https://github.com/pymumu/luci-app-smartdns package/lean/luci-app-smartdns
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus  package/luci-app-jd-dailybonus
