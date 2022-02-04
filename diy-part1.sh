ec38a5d958e2dde8bbfecbb88a5dd689651e7c85
ghtyn <81617033+ghtyn@users.noreply.github.com>
2022-02-04 15:50:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,7 +22,7 @@ git clone https://github.com/garypang13/luci-theme-edge  package/luci-theme-edge
 git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
 #git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 #git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 #git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
 #git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
